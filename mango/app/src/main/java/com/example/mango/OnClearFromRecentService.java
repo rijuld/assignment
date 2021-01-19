c1782b0e10ad334860caae8ba4578759bdfa0bd4
rijuld <errijuldahiya@gmail.com>
2021-01-19 22:15:52 +0530

    added to a new git repo

---
+++ mango/app/src/main/java/com/example/mango/OnClearFromRecentService.java
@@ -0,0 +1,38 @@
+package com.example.mango;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.IBinder;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+public class OnClearFromRecentService extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("ClearFromRecentService", "Service Started");
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("ClearFromRecentService", "Service Destroyed");
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        Log.e("ClearFromRecentService", "END");
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor=prefs.edit();
+        editor.putBoolean("firststart",true);
+        editor.apply();
+        stopSelf();
+    }
+}
